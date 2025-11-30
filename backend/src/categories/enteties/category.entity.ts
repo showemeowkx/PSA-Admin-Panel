c3a449c46e22023cdd2dc228efb1368a73ffb4bd
showemeowkx <sashacherepov55@gmail.com>
2025-11-30 18:21:22 +0100

    Feat: create basic enteties for croducts and categories

---
+++ backend/src/categories/enteties/category.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Product } from '../../products/entities/product.entity';
+
+@Entity()
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  ukrskladId: number;
+
+  @Column()
+  name: string;
+
+  @OneToMany(() => Product, (product) => product.category)
+  products: Product[];
+}
--- backend/src/products/entities/product.entity.ts
+++ backend/src/products/entities/product.entity.ts
@@ -1,4 +1,51 @@
-import { Entity } from 'typeorm';
+import { Category } from 'src/categories/enteties/category.entity';
+import {
+  Entity,
+  Column,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
 
 @Entity()
-export class Product {}
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  ukrskladId: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column({ nullable: true })
+  categoryId: number;
+
+  @ManyToOne(() => Category, (category) => category.products, {
+    onDelete: 'SET NULL',
+  })
+  @JoinColumn({ name: 'categoryId' })
+  category: Category;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  price: number;
+
+  @Column('decimal', { precision: 10, scale: 3, default: 0 })
+  stockQty: number;
+
+  @Column()
+  unitsOfMeasurement: string;
+
+  @Column()
+  imagePath: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
