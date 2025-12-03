a7291d29a7cf20f2d496d9e9e1cc3dd2424d627b
showemeowkx <sashacherepov55@gmail.com>
2025-12-03 17:10:52 +0100

    Feat: create basic files for authentication

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  create(@Body() createUserDto: UpdateUserDto) {
+    return this.authService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.authService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.authService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: CreateUserDto) {
+    return this.authService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.authService.remove(+id);
+  }
+}
---
+++ backend/src/auth/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Auth {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  password: string;
+
+  @Column({ unique: true })
+  phone: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ default: false })
+  isAdmin: boolean;
+}
