2bbc5adbb059fad7abdc96b4db7e99461c30d6a1
showemeowkx <sashacherepov55@gmail.com>
2025-12-23 19:05:13 +0100

    Feat: implement categories creation feature

--- backend/src/categories/categories.controller.ts
+++ backend/src/categories/categories.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
+import { AdminGuard } from 'src/admin.guard';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Post()
+  @UseGuards(AdminGuard)
+  create(@Body() createCategoryDto: CreateCategoryDto): Promise<void> {
+    return this.categoriesService.create(createCategoryDto);
+  }
 }
--- backend/src/categories/categories.service.ts
+++ backend/src/categories/categories.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Category } from './enteties/category.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TopologyDescriptionChangedEvent } from 'typeorm/browser';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(
+    @InjectRepository(TopologyDescriptionChangedEvent)
+    private categoryRepository: Repository<Category>,
+  ) {}
+
+  async create(createCategoryDto: CreateCategoryDto): Promise<void> {
+    const categoryEntity = this.categoryRepository.create({
+      ...createCategoryDto,
+      iconPath: '', // PLACEHOLDER
+    });
+
+    try {
+      await this.categoryRepository.save(categoryEntity);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('This category already exists');
+      }
+      throw new InternalServerErrorException(
+        `Failed to create a category: ${error.stack}`,
+      );
+    }
+  }
+}
