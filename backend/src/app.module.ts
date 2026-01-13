4eb7443e632da2794f66aa34d53da98210d414ef
showemeowkx <sashacherepov55@gmail.com>
2026-01-13 13:24:37 +0100

    Refactor: remove useless app service and app controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
 import { CartModule } from './cart/cart.module';
@@ -33,7 +31,5 @@ import { StoreModule } from './store/store.module';
 
     StoreModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
