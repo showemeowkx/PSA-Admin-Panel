7a1c099d3c1108dddce6863d9a8b5c29d362ae3c
showemeowkx <sashacherepov55@gmail.com>
2026-01-23 20:27:51 +0100

    Feat: implement file upload method in the service

--- backend/src/cloudinary/cloudinary.service.ts
+++ backend/src/cloudinary/cloudinary.service.ts
@@ -1,4 +1,37 @@
+/* eslint-disable @typescript-eslint/prefer-promise-reject-errors */
 import { Injectable } from '@nestjs/common';
+import { v2 as cloudinary } from 'cloudinary';
+import { UploadApiResponse, UploadApiErrorResponse } from 'cloudinary';
+import { Readable } from 'typeorm/platform/PlatformTools.js';
+
+export interface CloudinaryFile {
+  fieldname: string;
+  originalname: string;
+  encoding: string;
+  mimetype: string;
+  size: number;
+  buffer: Buffer;
+}
 
 @Injectable()
-export class CloudinaryService {}
+export class CloudinaryService {
+  uploadFile(
+    file: CloudinaryFile,
+  ): Promise<UploadApiResponse | UploadApiErrorResponse> {
+    return new Promise((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        { folder: 'vikte_uploads' },
+        (error, result) => {
+          if (error) return reject(error);
+          if (result) resolve(result);
+          else reject(new Error('Upload failed'));
+        },
+      );
+      const stream = new Readable();
+      stream.push(file.buffer);
+      stream.push(null);
+
+      stream.pipe(uploadStream);
+    });
+  }
+}
