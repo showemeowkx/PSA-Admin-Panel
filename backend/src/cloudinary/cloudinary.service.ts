ce550bf56711cb04a1b806d11729bb1a2c838062
showemeowkx <sashacherepov55@gmail.com>
2026-02-06 19:54:51 +0100

    Feat: add logging to Cloudinary service

--- backend/src/cloudinary/cloudinary.service.ts
+++ backend/src/cloudinary/cloudinary.service.ts
@@ -1,23 +1,36 @@
 /* eslint-disable @typescript-eslint/prefer-promise-reject-errors */
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { v2 as cloudinary } from 'cloudinary';
 import { UploadApiResponse, UploadApiErrorResponse } from 'cloudinary';
 import { Readable } from 'stream';
 
 @Injectable()
 export class CloudinaryService {
+  private logger = new Logger(CloudinaryService.name);
+
   uploadFile(
     file: Express.Multer.File,
   ): Promise<UploadApiResponse | UploadApiErrorResponse> {
+    this.logger.verbose('Uploading a file...');
+
     return new Promise((resolve, reject) => {
       const uploadStream = cloudinary.uploader.upload_stream(
         { folder: 'vikte_uploads' },
         (error, result) => {
-          if (error) return reject(error);
-          if (result) resolve(result);
-          else reject(new Error('Upload failed'));
+          if (error) {
+            this.logger.error(`Upload failed: ${error.stack}`);
+            return reject(error);
+          }
+          if (result) {
+            this.logger.error('File uploaded seccussfully!');
+            resolve(result);
+          } else {
+            this.logger.error('Upload failed: Unknown error');
+            reject(new Error('Upload failed'));
+          }
         },
       );
+
       const stream = new Readable();
       stream.push(file.buffer);
       stream.push(null);
@@ -27,6 +40,10 @@ export class CloudinaryService {
   }
 
   async deleteFile(url: string): Promise<void> {
+    this.logger.verbose(
+      `Deleting a file from cloud... {url: ${url.slice(15)}...}`,
+    );
+
     const publicId = this.extractPublicId(url);
     if (publicId) {
       await cloudinary.uploader.destroy(publicId);
