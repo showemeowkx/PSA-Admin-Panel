23d7bf3bf3501805fd5a70a0c3022de1b82f1934
showemeowkx <sashacherepov55@gmail.com>
2026-02-10 18:18:10 +0100

    Refactor (categories): discard changes in update category DTO

--- backend/src/categories/dto/update-category.dto.ts
+++ backend/src/categories/dto/update-category.dto.ts
@@ -1,22 +1,8 @@
-import {
-  IsBoolean,
-  IsOptional,
-  IsString,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { IsBoolean, IsOptional } from 'class-validator';
+import { CreateCategoryDto } from './create-category.dto';
 
-export class UpdateCategoryDto {
-  @IsString()
-  @MinLength(2)
-  @MaxLength(30)
-  @IsOptional()
-  name?: string;
-
-  @IsOptional()
-  @IsString()
-  iconPath?: string;
-
+export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
   @IsBoolean()
   @IsOptional()
   isActive?: boolean;
